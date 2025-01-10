923385ec5e36edd00f3e764d430129c172009029
Jeremias Rivelli <jeremias.j.riv@gmail.com>
2025-01-10 16:25:49 -0300

    Frontend funcional nuevamente

--- FrontEnd/src/api/axiosConfig.ts
+++ FrontEnd/src/api/axiosConfig.ts
@@ -11,7 +11,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://192.168.1.16:8080/api", // URL base del backend
+  baseURL: "http://149.56.68.32:8080/api", // URL base del backend
   headers: {
     "Content-Type": "application/json", // Configura el tipo de contenido como JSON
   },
