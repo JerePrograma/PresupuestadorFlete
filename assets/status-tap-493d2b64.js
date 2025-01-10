7732ce7b375aaa0860cd0042cf9d8eeaeeb196cb
Jeremias Rivelli <jeremias.j.riv@gmail.com>
2025-01-10 11:09:16 -0300

    Deploy Frontend

---
+++ assets/status-tap-493d2b64.js
@@ -0,0 +1,4 @@
+import{r as i,f as a,b as c,w as d,s as l}from"./index-41688c40.js";import"@ionic/core/components";/*!
+ * (C) Ionic http://ionicframework.com - MIT License
+ */const p=()=>{const e=window;e.addEventListener("statusTap",()=>{i(()=>{const o=e.innerWidth,s=e.innerHeight,n=document.elementFromPoint(o/2,s/2);if(!n)return;const t=a(n);t&&new Promise(r=>c(t,r)).then(()=>{d(async()=>{t.style.setProperty("--overflow","hidden"),await l(t,300),t.style.removeProperty("--overflow")})})})})};export{p as startStatusTap};
+//# sourceMappingURL=status-tap-493d2b64.js.map
